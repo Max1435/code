e7ed3b5dc87d4dbc4c5b96abe1d67837bfc54196
Max1435 <maxsturm29@gmail.com>
2016-02-13 16:25:32 +0000

    First commit

---
+++ calc.rb
@@ -0,0 +1,11 @@
+puts "Welcome to Beefy Winterfell"
+puts "Enter a number"
+number1 = gets.chomp.to_i
+puts "What operation do you want to use?"
+operation = gets.chomp
+puts "Enter a second number"
+number2 = gets.chomp.to_i
+if operation == "*"
+  answer = number1 * number2
+end
+puts "#{number1} #{operation} #{number2} equals #{answer}"
