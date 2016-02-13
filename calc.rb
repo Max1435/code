9896b938723cf73953a584679def43d15d238b61
Max1435 <maxsturm29@gmail.com>
2016-02-13 16:31:30 +0000

    second commit

--- calc.rb
+++ calc.rb
@@ -1,11 +1,17 @@
 puts "Welcome to Beefy Winterfell"
 puts "Enter a number"
-number1 = gets.chomp.to_i
+number1 = gets.chomp.to_f
 puts "What operation do you want to use?"
 operation = gets.chomp
 puts "Enter a second number"
-number2 = gets.chomp.to_i
+number2 = gets.chomp.to_f
 if operation == "*"
   answer = number1 * number2
+elsif operation == "+"
+  answer = number1 + number2
+elsif operation == "-"
+  answer = number1 - number2
+elsif operation == "/"
+  answer = number1 / number2
 end
 puts "#{number1} #{operation} #{number2} equals #{answer}"
